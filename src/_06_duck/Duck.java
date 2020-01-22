50a4c604d828f2a99ff85d1c9f55dc62d5ea96f8
League Student <league@iMac-16.attlocal.net>
2020-01-21 18:07:04 -0800

    I started the Duck thing and almost finished it

---
+++ src/_06_duck/Duck.java
@@ -0,0 +1,22 @@
+package _06_duck;
+
+public class Duck {
+	
+
+int	numberOfFriends;
+String favoriteFood;
+Duck(String favoriteFood, int numberOfFriends) {
+    this.favoriteFood = favoriteFood;
+    this.numberOfFriends = numberOfFriends;
+}
+void quack() {
+	
+	
+}
+void waddle() {
+
+}
+	
+	
+	
+}
---
+++ src/_06_duck/FluffyCow.java
@@ -0,0 +1,26 @@
+package _06_duck;
+
+public class FluffyCow {
+
+	
+	String name;
+	int FluffCount;
+	
+	FluffyCow(String name, int FluffCount){
+		this.name = name;
+		this.FluffCount=FluffCount;
+		
+	}
+	
+	void moo() {
+		System.out.println("moo");
+	}
+	void eatGrass() {
+	System.out.println("chomp");
+	}
+	
+	
+	
+	
+	
+}
